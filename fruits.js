ea5ee9bb9b4ffe8610b19fbe309c2ec930411101
Rubel <mr.adcadre41@gmail.com>
2026-01-11 17:05:46 +0600

    fruits-array

---
+++ fruits.js
@@ -0,0 +1,4 @@
+const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+console.log(fruits[2]); // Accessing the third fruit
+fruits[1] = "jambura"; // Modifying the second fruit
+console.log(fruits);
